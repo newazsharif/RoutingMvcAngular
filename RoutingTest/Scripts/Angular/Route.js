11205cee8bde2229224a80e254d7a29129a5abf8
newazsharif <newaz.sharif@hotmail.com>
2015-08-02 16:18:35 +0600

    First commit

---
+++ RoutingTest/Scripts/Angular/Route.js
@@ -0,0 +1,60 @@
+﻿//var myApp = angular.module('MyApp', ['ngRoute']);
+//myApp.config(function ($routeProvider) {
+//    $routeProvider
+//    .when('/',
+//    {
+//        templateUrl: 'Home'
+//    })
+//    .when('/index',
+//    {
+//        templateUrl: '/Home/index'
+//    })
+//    when('/About',
+//    {
+//        templateUrl: '/Home/About'
+//    })
+//    when('/Contact',
+//    {
+//        templateUrl: 'Home/Contact'
+//    })
+//    .otherWise
+//    {
+//        templateUrl: 'Home'
+//    };
+//});
+
+
+angular.module('MyApp', ['ngRoute'])
+.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: 'Home/About'
+        })
+        // route for the home page
+        .when('/Login', {
+            templateUrl: 'Account/Login'
+
+        })
+
+        // route for the about page
+        .when('/About', {
+            templateUrl: 'Home/About'
+
+        })
+
+        // route for the contact page
+        .when('/Contact', {
+            templateUrl: 'Home/Contact'
+
+        })
+    .otherWise
+        {
+            redirectTo: "/"
+        };
+    //$locationProvider.html5Mode(true).hashPrefix('!')
+
+});
+
+
+
